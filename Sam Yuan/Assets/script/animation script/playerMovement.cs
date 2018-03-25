4c3efe7048f8e223c915901bdfe2d22c9aec1ecf
turfnima <lxzno3@gmail.com>
2018-03-25 12:43:51 -0400

    animation: running, jumping, idle

---
+++ Sam Yuan/Assets/script/animation script/playerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+	//this to get the animator
+	public Animator anim;
+
+
+	//some input getters
+	private float inputH;
+	private float inputV;
+
+
+	// Use this for initialization
+	void Start () {
+		//get the animator.
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("space")) {
+			//-1 for the layer base layer
+			//0f is when how far through the animation do we switch
+			anim.Play ("jump",-1,0f);
+		}
+		else if (Input.GetKey ("2")) {
+			anim.Play ("stop",-1,0f);
+		}
+
+		inputH = Input.GetAxis ("Horizontal");
+		inputV = Input.GetAxis ("Vertical");
+
+		anim.SetFloat ("inputH", inputH);
+		anim.SetFloat ("inputV", inputV);
+	}
+}
