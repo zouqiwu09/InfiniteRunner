ab496be9a784a2d0edd10b3276a0b460139f78b0
Muzhen Li <muzhenli@Muzhens-MacBook-Air.local>
2018-04-20 17:53:13 -0400

    Sound Effect MuzhenLi

---
+++ Assets/PlayerSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour {
+
+	public AudioClip jumpSound;
+
+	public AudioSource source;
+	public float volLowRange = .5f;
+	public float volHighRange = 1.0f;
+
+	// Use this for initialization
+	void Awake () {
+
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Jump")) 
+		{
+			source.PlayOneShot (jumpSound, 1F);
+			
+		}
+		
+	}
+}
