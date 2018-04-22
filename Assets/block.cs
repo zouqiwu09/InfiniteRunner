4581e257ec5f39caef607ed18bb2bfd6379242c7
Yuan <zyuan04@syr.edu>
2018-04-22 04:27:45 -0400

    block and auto block creation(still have bugs)

---
+++ Assets/block.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+using System;
+
+public class block : MonoBehaviour {
+    bool touch_by_user = false;
+    RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+    // Use this for initialization
+    void Start () {
+        byte[] bs = new byte[8];
+        rng.GetBytes(bs);
+        int value = BitConverter.ToInt32(bs, 0);
+        value = value % 10;
+        if (value < 0) value = -value;
+        // safe random number
+
+        if (value < 20)
+        {
+            Destroy(this);
+        }
+        else touch_by_user = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this);
+    }
+    private void OnDestroy()
+    {
+        
+    }
+}
