29c45a1caf9f5435fbd81f5aacaa4acc4929518f
YuqingGu <g876940333@gmail.com>
2018-05-04 06:19:27 +0800

    add comment

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿//Yuqing Gu wrote most of the code on 4/08/18
+//Bug-Fixes were accomplished by Qiwu Zou on 4/08/18
+//Final unit testing were accomplished by Qiwu Zou and Zehu Yuan on 4/29/18
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
